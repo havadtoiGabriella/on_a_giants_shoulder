43cb867909f11903681b6cce96f99cb1431a2704
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-07-21 12:09:27 +0200

    Driver class added

--- src/main/java/com/giantshoulder/driver/Driver.java
+++ src/main/java/com/giantshoulder/driver/Driver.java
@@ -1,24 +1,21 @@
 package com.giantshoulder.driver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-
-/**
- * @author Gabriella_Havadtoi
- */
 
 public class Driver {
     private static ChromeDriver driver;
 
-    @BeforeTest
-    public static void setUpDriver() {
+    public static ChromeDriver getChromeDriver() {
+        return driver;
+    }
+
+    public static void initDriver() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver/chromedriver.exe");
         driver = new ChromeDriver();
     }
 
-    @AfterTest
     public static void closeDriver() {
         driver.quit();
     }
 }
+
