7e10b9665d692268601505913831d793d73aef7e
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-07-22 16:50:05 +0200

    parameterized the feature file

--- src/test/java/stepdefinition/SuccessfulSubscriptionStep.java
+++ src/test/java/stepdefinition/SuccessfulSubscriptionStep.java
@@ -10,28 +10,23 @@ public class SuccessfulSubscriptionStep extends BaseStepDefinition {
 
     SuccessfulSubscriptionPage successfulSubscriptionPage = new SuccessfulSubscriptionPage(driver);
 
-    @Then("I should get a successful subscription message for learn faster")
-    public void i_should_get_a_successful_subscription_message_for_learn_faster() {
-        String message = " Your email is confirmed and the FREE download will arrive in your inbox shortly.";
+    @Then("I should get a successful subscription {string} for learn faster")
+    public void i_should_get_a_successful_subscription_message_for_learn_faster(String message) {
         successfulSubscriptionPage.validateSuccessMessageFor("Learn faster", message);
     }
 
-    @Then("I should get a successful subscription message for stop procrastinating")
-    public void i_should_get_a_successful_subscription_message_for_stop_procrastinating() {
-        String message = " Your email is confirmed and the FREE download will arrive in your inbox shortly.";
+    @Then("I should get a successful subscription {string} for stop procrastinating")
+    public void i_should_get_a_successful_subscription_message_for_stop_procrastinating(String message) {
         successfulSubscriptionPage.validateSuccessMessageFor("Stop procrastinating", message);
     }
 
-    @Then("I should get a successful subscription message for reach your goals")
-    public void i_should_get_a_successful_subscription_message_for_reach_your_goals() {
-        String message = "Thanks for Subscribing!";
+    @Then("I should get a successful subscription {string} for reach your goals")
+    public void i_should_get_a_successful_subscription_message_for_reach_your_goals(String message) {
         successfulSubscriptionPage.validateSuccessMessageFor("Reach your goals", message);
     }
 
-    @Then("I should get a successful subscription message for improve focus")
-    public void i_should_get_a_successful_subscription_message_for_improve_focus() {
-        String message = "Thanks for Subscribing!";
+    @Then("I should get a successful subscription {string} for improve focus")
+    public void i_should_get_a_successful_subscription_message_for_improve_focus(String message) {
         successfulSubscriptionPage.validateSuccessMessageFor("Improve focus", message);
     }
-
 }
