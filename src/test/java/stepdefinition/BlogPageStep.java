e7374c3ba35c33dc3f5f494102ce8712ebb0d87c
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-07-29 09:27:18 +0200

    added type registry config

--- src/test/java/stepdefinition/BlogPageStep.java
+++ src/test/java/stepdefinition/BlogPageStep.java
@@ -6,10 +6,14 @@ import cucumber.api.java.en.Given;
 
 public class BlogPageStep extends BaseStepDefinition {
 
-    @Given("I navigate to the {String} blog page")
+    @Given("I navigate to the {pageType} blog page")
     public void i_navigate_to_the_x_blog_page(PageType pageType) {
         HomePage homePage = new HomePage(driver);
         homePage.clickOnBlogMenuItem();
         homePage.clickOnBlogPage(pageType);
+    }
+
+    @Given("I navigate to the {string} blog page")
+    public void iNavigateToTheBlogPage(String arg0) {
     }
 }
