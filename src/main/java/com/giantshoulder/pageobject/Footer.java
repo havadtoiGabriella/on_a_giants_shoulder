e6ace0737c79f0a01f0738b33e5f5fbef8eef0f7
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-08-01 14:48:41 +0200

    removed unused import

--- src/main/java/com/giantshoulder/pageobject/Footer.java
+++ src/main/java/com/giantshoulder/pageobject/Footer.java
@@ -4,7 +4,6 @@ import static com.giantshoulder.util.WebElementAssertions.elementHasCorrectUrl;
 import static com.giantshoulder.util.WebElementAssertions.elementIsDisplayed;
 import static com.giantshoulder.util.WebElementHandler.*;
 import java.util.List;
-import com.giantshoulder.util.WebElementHandler;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
